bcc371b0718855e0adb5fa41c84f3ae42e4fa8c7
smm3123 <syedmahdi3123@gmail.com>
2024-03-10 09:30:01 -0500

    Make chart responsive

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { isEmpty } from 'lodash';
 import { Box } from '@mui/material';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 const EmptyState = () => <Box>Fill in some values to get started</Box>;
 
@@ -14,29 +14,33 @@ const Visualization = ({ yearlyReturns }) => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        width: '100%',
+        height: '100%',
       }}
     >
       <Box sx={{ fontWeight: "bold", fontSize: '20px', mb: '20px' }}>
         {`You will have $${finalYear.amount} by ${finalYear.year}`}
       </Box>
-      <LineChart
-        width={800}
-        height={600}
-        data={yearlyReturns}
-        margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" />
-        <YAxis />
-        <Tooltip />
-        <Line type="monotone" dataKey="amount" stroke="#4841cc" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="totalContributions" stroke="#9c2624" activeDot={{ r: 8 }} />
-      </LineChart>
+      <ResponsiveContainer width="90%" height="90%">
+        <LineChart
+          width={800}
+          height={600}
+          data={yearlyReturns}
+          margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="year" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="amount" stroke="#4841cc" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="totalContributions" stroke="#9c2624" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
     </Box>
   )
 }
