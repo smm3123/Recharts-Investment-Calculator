b2bf833c4230c213e5907e3196db4f93391691a7
smm3123 <syedmahdi3123@gmail.com>
2024-09-30 22:52:48 -0500

    Add useIsMobile hook

---
+++ src/hooks/useIsMobile.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+const useCheckMobileScreen = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  
+  const handleWindowSizeChange = () => {
+          setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
+  return (width <= 768);
+}
+
+export default useCheckMobileScreen;
